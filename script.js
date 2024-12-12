b272c3879b434151f9b173af8fdaf70664380312
Priya R Rajbhar <induram4321@gmail.com>
2024-12-13 01:03:30 +0530

    page 1 made

--- script.js
+++ script.js
@@ -38,28 +38,37 @@ tl.to("#loader",{
 tl.from("#page1",{
     delay: 0.1,
     y: 1600,
-    duration: 0.5,
+    duration: 0.6,
     ease: Power4
 });
 
 tl.to("#loader",{
     display: "none"
 });
+
+tl.from("#nav",{
+    opacity: 0
+})
+
+tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1",{
+    y:120,
+    stagger: 0.2
+})
+}
+
+
+function cursorAnimation(){
+    document.addEventListener("mousemove", function(dets){
+        gsap.to("#crsr", {
+            x: dets.x,
+            y: dets.y
+        });
+    });
+    
+    Shery.makeMagnet("#nav-part2 h4");
 }
 loadingAnimation()
-
-document.addEventListener("mousemove", function(dets){
-    gsap.to("#crsr", {
-        x: dets.x,
-        y: dets.y
-    });
-});
-
-Shery.makeMagnet("#nav-part2 h4", {
-    skew: true,
-    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-    duration: 1,
-  });
+cursorAnimation()
 
 
 
