df69f0fc5925d12fde6b567a60001e5e192fd2e9
Priya R Rajbhar <induram4321@gmail.com>
2024-12-18 03:54:51 +0530

    All bugs are clear

--- script.js
+++ script.js
@@ -92,12 +92,59 @@ tl.from("#hero1 h1, #page2",{
 }
 
 function cursorAnimation() {
-    Shery.mouseFollower({
-      skew: true,
-      ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-      duration: 1,
-    });
+        Shery.mouseFollower({
+            //Parameters are optional.
+            skew: true,
+            ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+            duration: 1,
+          });
     Shery.makeMagnet("#nav-part2 h4");
+
+    var videoContainer = document.querySelector("#video-container");
+    var video = document.querySelector("#video-container video")
+    videoContainer.addEventListener("mouseenter", function () {
+      videoContainer.addEventListener("mousemove", function (dets) {
+        gsap.to(".mousefollower", {
+          opacity: 0
+        });
+        gsap.to("#video-cursor", {
+          left: dets.x - 570,
+          y: dets.y - 300,
+        });
+      });
+    });
+    videoContainer.addEventListener("mouseleave", function () {
+      gsap.to(".mousefollower", {
+        opacity: 1
+  
+      });
+      gsap.to("#video-cursor", {
+        left: "70%",
+        top: "-15%",
+      });
+    });
+  
+  
+    var flag = 0
+    videoContainer.addEventListener("click", function () {
+      if (flag == 0) {
+        video.play()
+        video.style.opacity = 1
+        document.querySelector("#video-cursor").innerHTML = `<i class="ri-pause-mini-fill"></i>`
+        gsap.to("#video-cursor", {
+          scale: 0.5
+        })
+        flag = 1
+      } else {
+        video.pause()
+        video.style.opacity = 0
+        document.querySelector("#video-cursor").innerHTML = `<i class="ri-play-mini-fill"></i>`
+        gsap.to("#video-cursor", {
+          scale: 1
+        })
+        flag = 0
+      }
+    })
 }
 
 function sheryAnimation(){
@@ -107,10 +154,36 @@ function sheryAnimation(){
         gooey: true
     })
 }
+
+function flag(){
+    document.addEventListener("mousemove", function(dets){
+        gsap.to("#flag",{
+            x:dets.x,
+            y:dets.y
+        })
+    })
+    
+    document.querySelector("#hero3").addEventListener("mouseenter",function(){
+        gsap.to("#flag",{
+            opacity:1
+        })
+    })
+    
+    document.querySelector("#hero3").addEventListener("mouseleave",function(){
+        gsap.to("#flag",{
+            opacity:0
+        })
+    })
+}
+
 locomotiveAnimation()
 loadingAnimation()
 cursorAnimation()
 sheryAnimation()
+flag()
+
+
+
 
 
 
